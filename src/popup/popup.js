305c001cf4a5226a5ade41d3f4955681837c42f2
Destiny Saturday <yiradesat@gmail.com>
2023-04-11 07:57:24 +0100

    plans changed, new design, light and dark implemeted on poup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,24 @@
-const switchMode = () => {
-    
+const whiteButton = document.getElementById("light-hero");
+const blackButton = document.getElementById("dark-knight");
+
+const body = document.body;
+var className = document.body.className;
+const theme = localStorage.getItem('theme');
+
+if (theme) {
+    body.classList.replace(className, theme);
+    className = theme;
 }
 
-document.querySelector("#toggle-slider").addEventListener('click', switchMode)
+whiteButton.onclick = () => {
+    body.classList.replace(className, 'light-hero');
+    className = 'light-hero';
+    localStorage.setItem('theme', 'light-hero');
+}
+
+blackButton.onclick = () => {
+    body.classList.replace(className, 'dark-knight');
+    className = 'dark-knight';
+    localStorage.setItem('theme', 'dark-knight');
+
+}
